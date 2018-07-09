d2c6efcd5a67b5dbc37ebea48264f7ffc16eb177
yash2798 <yash.raj270998@gmail.com>
2018-07-10 03:02:37 +0530

    Updated remote repository

---
+++ aws_dynamodb.py
@@ -0,0 +1,80 @@
+#The code reads the input from output.txt and inserts it in the dynamoDb
+
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+import json
+import decimal
+import fcntl
+import struct
+import time
+import ssl
+import datetime
+import calendar
+
+userId = "amzn1.ask.account.AHFIXJPV6S446WKJYWB7QLF76DF2YLLPNX3EP2DQQW664LET5TN2XM3ZB24L5B2PTTV6MMRTYY4IPBYMGL6V7YJNLI7OAF6FUSCZWJOCL5423IOIYSFRJCMJ5GSVXFLJRDVKDUIOAQ73HHKF3K4GW4YCWA5XYXYSFDNIIQH6VQPNHLREM5WT5X4KLEDCVLA6IU4GPZMZBI4K5ZI"
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+#Update the item on AWS
+#For that we need guid, timestamp and message
+def call_aws(message):
+	now = datetime.datetime.utcnow()
+	timestamp = int(round((now - datetime.datetime(2016, 1 , 1)).total_seconds()))
+	#days = (now - datetime.datetime(2016, 1 , 1)).days + id
+	dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="https://dynamodb.us-east-1.amazonaws.com")
+	table = dynamodb.Table('smartcap')
+	# your logic here...
+	try:
+		print("Trying to print " + message)
+		response = table.update_item(
+		Key={
+		     'guid' : userId
+		     #'command' : message,
+		    },
+		    UpdateExpression="set tstamp= :t, command =:r",
+		    ExpressionAttributeValues={
+		    ':r': message,
+		    ':t': str(timestamp) 
+		    },
+		    ReturnValues="UPDATED_NEW"
+		)
+		print("PutItem succeeded:")
+		return 1
+	except Exception, e:
+		print (e)
+		fol = open("awserror.txt", "wb")
+		fol.write(str(e))
+		fol.close()
+		return e
+
+#print(json.dumps(response, indent=4, cls=DecimalEncoder))
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+#Main function
+def main():
+	ssl._create_default_https_context = ssl._create_unverified_context
+	#Open the output.txt file in the read mode"
+	fo = open("output.txt", "rt")
+	try:			
+		e = call_aws(str(fo.read()))						
+	except Exception, e:
+		print (e)
+		time.sleep(.5)
+# Close opend file
+	fo.close()
+
+
+if __name__ == "__main__":
+    main()
+    
---
+++ ms_visionapi.py
@@ -0,0 +1,89 @@
+
+########### Python 2.7 #############
+import httplib, urllib, base64
+import signal
+import numpy as np
+import sys
+import json
+
+#To handle the SIGINT when CTRL+C is pressed
+def exit_gracefully(signum,frame):
+    signal.signal(signal.SIGINT, original_sigint)
+    sys.exit(1)
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key': '58352edfcec14381a903bc44df00d40e',
+}
+
+params = urllib.urlencode({
+    # Request parameters
+    'visualFeatures': 'Description',
+})
+
+#Saves the text to the file
+def saveTextFile(text):
+    try:
+        print(text)
+        text_file = open("output.txt","w+")
+        text_file.write(text)
+        text_file.close()
+    except Exception, e:
+        print ("Exception occured \n")
+        print (e)
+        pass
+
+def read_image():
+    pathToFileInDisk = r'smart_cap.png'
+    with open(pathToFileInDisk, 'rb') as f:
+        data = f.read()
+    return data
+
+def analyze_image(data):
+    try:
+        conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        conn.request("POST", "/vision/v1.0/analyze?%s" % params, data, headers)
+        response = conn.getresponse()
+        data = response.read()
+        print(data)
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+    return data
+
+def tag_from_data(input):
+    if input is not None:
+        # Load the original image, fetched from the URL
+        # data8uint = np.fromstring(input, np.uint8)  # Convert string to an unsigned int array
+        # Get the description/tag
+        result = json.loads(input)
+        description = result['description']['captions'][0]['text']
+        print(data)
+        # img = cv2.cvtColor(cv2.imdecode(data8uint, cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
+
+        # columns = defaultdict(list)
+
+        # Get image captipn and keywords
+        # Concatenate them to form a single string
+        awsstring = "I think it is "
+        awsstring += description
+        awsstring += ". And the keywords are  "
+        num_keywords = 5
+        for i in range(num_keywords):
+            awsstring += result['description']['tags'][i]
+            if i != num_keywords - 1:
+                awsstring += ', '
+
+        return awsstring
+
+
+if __name__ == '__main__':
+    original_sigint = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT,exit_gracefully)
+    img = read_image()
+    data = analyze_image(img)
+    text = tag_from_data(data)
+    saveTextFile(text)
+####################################
